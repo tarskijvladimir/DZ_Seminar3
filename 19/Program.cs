4155308892a5d5927be1962a1c3e0094d2ffde06
vladimir <tarskij-vladimir@yandex.ru>
2023-09-26 21:18:41 +0300

    added19

---
+++ 19/Program.cs
@@ -0,0 +1,20 @@
+﻿int num;
+int r;
+int sum=0;
+int t;
+Console.Write("Введите пятизначное число: ");
+num = Convert.ToInt32(Console.ReadLine());
+if(num>9999 && num<100000)
+{
+for(t=num; num!=0; num=num/10)
+    {
+    r=num % 10;
+    sum=sum*10+r; // две цифры
+    }
+if(t==sum)
+    Console.Write($"{t} является палиндромом.");
+else
+    Console.Write($"{t} не является палиндромом.");
+} 
+else
+    Console.Write("Введите пятизначное число! ");
