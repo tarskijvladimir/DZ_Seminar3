7e4ca088ae86668351e8be3e5d363f6492aaa0b4
vladimir <tarskij-vladimir@yandex.ru>
2023-09-26 21:20:38 +0300

    added 23

---
+++ 23/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+{
+    double pow1 = Math.Pow(i, 3); // куб числа
+    Console.Write($"{pow1}, ");
+}
+Console.Write("\b\b ");  // убираем последнюю запятую
+
