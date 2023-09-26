448460b2d08cf2921441fdd05868175e00c66c55
vladimir <tarskij-vladimir@yandex.ru>
2023-09-26 21:20:06 +0300

    added 21

---
+++ 21/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите координаты X, точки А: ");
+int x1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты Y, точки А: ");
+int y1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты Z, точки А: ");
+int z1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты X, точки B: ");
+int x2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты Y, точки B: ");
+int y2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты Z, точки B: ");
+int z2 = int.Parse(Console.ReadLine());
+
+
+double c = Math.Sqrt (Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2) + Math.Pow((z1 - z2), 2));
+
+Console.WriteLine($"Расстояние между координатами: {c} ");
+
